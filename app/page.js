7ea4619fdb5f0914f23c4b3ce9f008a7028f12ce
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-09-29 16:28:43 +1000

    Fixing linting errors/warnings

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 'use client';
-
-import Image from 'next/image';
-import { Divider } from "@nextui-org/react";
+// import Image from 'next/image';
+// import { Divider } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg justify-center">
         <div>
-          <h1>Hi, I'm Jeremy Traini. More to come.</h1>
+          <h1>Hi, I&apos;m Jeremy Traini. More to come.</h1>
         </div>
       </div>
       {/* <Divider /> */}
