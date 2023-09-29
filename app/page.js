c6bcfa56c254567b1b417556b860241cdabfccfd
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-09-29 15:07:26 +1000

    Adding basic navbar, footer and page layouts

--- app/page.js
+++ app/page.js
@@ -2,16 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          
-          <p className="w-full justify-center">
-            Check back soon...
-          </p>
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <div>
+          <h1>Home</h1>
         </div>
       </div>
-      
-    </main>
+    </section>
   )
 }
---
+++ config/site.js
@@ -0,0 +1,11 @@
+
+export const siteConfig = {
+	name: "Jeremy Traini",
+	description: "My website",
+	links: {
+		github: "https://github.com/jeremytraini",
+		linkedin: "https://www.linkedin.com/in/jeremy-traini-884a56174/"
+	},
+	navItems: [],
+	navMenuItems: []
+};
