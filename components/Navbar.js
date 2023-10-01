9c1fb02280af840e8c7f015f2757f28730f74b06
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-02 10:18:00 +1100

    Did work on project cards

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,10 @@
 import {
 	Navbar as NextUINavbar,
 	NavbarContent,
-	NavbarMenu,
-	NavbarMenuToggle,
 	NavbarBrand,
 	NavbarItem,
-	NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 
 import { link as linkStyles } from "@nextui-org/theme";
 
