5ef80c72989c3fd929986ed09ff0e224adac451f
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-02 10:18:00 +1100

    Did work on project cards

--- app/page.js
+++ app/page.js
@@ -1,6 +1,16 @@
 'use client';
-// import Image from 'next/image';
-// import { Divider } from "@nextui-org/react";
+import { Divider } from "@nextui-org/react";
+import ProjectCard from "@/components/ProjectCard";
+
+const projects = [
+  {
+    title: "Project 1",
+    description: "Description for project 1.",
+    imageUrl: "https://placehold.co/600x100/png",
+    projectLink: "https://example.com/project1"
+  },
+  
+];
 
 export default function Home() {
   return (
@@ -10,12 +20,15 @@ export default function Home() {
           <h1>Hi, I&apos;m Jeremy Traini. More to come.</h1>
         </div>
       </div>
-      {/* <Divider /> */}
-      {/* <div className="inline-block max-w-lg text-center justify-center">
-        <div>
-          <h2>Work</h2>
-        </div>
-      </div> */}
+      <Divider />
+      <div className="inline-block max-w-lg text-left">
+        <h2>Projects</h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map((project) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
+      </div>
     </section>
   )
 }
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -3,10 +3,16 @@ import Image from 'next/image';
 export default function ProjectCard({ project }) {
   return (
     <div className="border rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300">
-      <Image src={project.imageUrl} alt={project.title} className="w-full h-48 object-cover" />
+      <Image
+        src={project.imageUrl}
+        alt={project.title}
+        className="w-full h-48 object-cover"
+        width={400}
+        height={200}
+      />
       <div className="p-4">
         <h2 className="text-xl mb-2">{project.title}</h2>
-        <p className="text-gray-600 mb-4">{project.description}</p>
+        <p className="text-gray-600 dark:text-gray-300 mb-4">{project.description}</p>
         <a href={project.projectLink} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">View Project</a>
       </div>
     </div>
