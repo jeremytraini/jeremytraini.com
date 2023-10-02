c82fb671d2ee0094b1e4530869b290d213d9cb66
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-03 00:14:49 +1100

    Implementation of INTERACTIVE infinite scrolling brand chips

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import { Card, CardHeader, CardBody, CardFooter, Image } from "@nextui-org/react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+import Flickity from 'react-flickity-component';
 import BrandChip from "./BrandChip";
 
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
+import "flickity/css/flickity.css";
 
-import { FreeMode, Pagination } from 'swiper/modules';
+const flickityOptions = {
+  initialIndex: 3,
+  wrapAround: true,
+  freeScroll: true,
+  contain: true,
+  prevNextButtons: false,
+  // selectedAttraction: 1,
+  // friction: 1,
+  // freeScrollFriction: 0.05,
+  // autoPlay: true,
+  // autoPlay: 3000,
+  // pauseAutoPlayOnHover: true,
+}
+
+const Carousel = ({ technologies }) => {
+  return (
+    <Flickity
+      elementType="div"
+      options={flickityOptions}
+      // disableImagesLoaded={false}
+      // reloadOnUpdate
+    >
+      {technologies.map((tech, index) => (
+        <div
+          key={index}
+          className="mr-2"
+        >
+          <BrandChip brand={tech} />
+        </div>
+      ))}
+    </Flickity>
+  );
+}
 
 export default function ProjectCard({ project, ...props }) {
   return (
@@ -29,37 +59,16 @@ export default function ProjectCard({ project, ...props }) {
       <CardFooter>
         <div className="max-w-full">
           <small className="text-default-500">{project.description}</small>
-          {project.technologies &&
+          {project.technologies && (
             <>
               <h5 className="font-bold text-medium py-2">Tech used</h5>
-              <Swiper
-                // scrollbar={{
-                //   hide: true,
-                // }}
-                slidesPerView='auto'
-                spaceBetween={10}
-                centeredSlides={true}
-                autoplay={{
-                  delay: 1000,
-                  disableOnInteraction: false,
-                }}
-                loop={true}
-                loopedSlides={4}
-                freeMode={true}
-                modules={[FreeMode, Pagination]}
+              <div
+                className="max-h-16 overflow-y-hidden"
               >
-                <SwiperSlide><BrandChip brand="React.js" /></SwiperSlide>
-                <SwiperSlide><BrandChip brand="React.js2" /></SwiperSlide>
-                <SwiperSlide><BrandChip brand="React.js3" /></SwiperSlide>
-                <SwiperSlide><BrandChip brand="React.js4" /></SwiperSlide>
-                {/* {project.technologies.map((tech, index) => (
-                  <SwiperSlide key={index}>
-                    <BrandChip brand={tech} />
-                  </SwiperSlide>
-                ))} */}
-              </Swiper>
+                <Carousel technologies={project.technologies} />
+              </div>
             </>
-          }
+          )}
         </div>
       </CardFooter>
     </Card>
