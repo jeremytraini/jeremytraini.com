6b553924eab26878477f9abbdf1e6b9d4bd0ae53
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-03 08:06:39 +1100

    Adding more info on projects

--- data/projects.js
+++ data/projects.js
@@ -4,25 +4,33 @@ const projects = [
     title: "Tekkerz PDP",
     subtitle: "Player Development Platform",
     description: "A soccer coaching app that seamlessly integrates player engagement, skill-building, and performance tracking, enabling players to master soccer skills at home with real-time feedback from their coaches and peers.",
+    technologies: ["Figma", "React Native", "ExpressJS", "Firebase"],
+    imageUrl: "https://placehold.co/600x100/png",
+    projectLink: "https://example.com/project1"
+  },
+  {
+    title: "ChurroFlow.com",
+    subtitle: "Warehouse Analytics Dashboard",
+    description: `Lead a team to develop an innovative platform that transforms routine invoices into actionable business intelligence using data analytics, machine learning and AI.
+                  ChurroFlow was presented to a board of Macquarie Group engineers, securing first place for the Software Engineering Macquarie Prize.
+                  `,
     technologies: ["React.js", "Python", "FastAPI", "PostgreSQL", "AWS" , "Jira", "Confluence"],
     imageUrl: "https://placehold.co/600x100/png",
     projectLink: "https://example.com/project1"
   },
   {
-    title: "Project 2",
-    description: "Description for project 2.",
-    imageUrl: "https://placehold.co/600x100/png",
-    projectLink: "https://example.com/project1"
-  },
-  {
     title: "Project 3",
-    description: "Description for project 3.",
+    subtitle: "Gamified Financial Education App Prototype",
+    description: "Worked in a team to develop an innovative educational platform designed to empower university students with comprehensive financial knowledge.",
+    technologies: ["Figma", "Jira", "Confluence"],
     imageUrl: "https://placehold.co/600x100/png",
     projectLink: "https://example.com/project1"
   },
   {
-    title: "Project 4",
-    description: "Description for project 4.",
+    title: "jeremytraini.com",
+    subtitle: "My website.",
+    description: "The website you're on right now.",
+    technologies: ["Next.js"],
     imageUrl: "https://placehold.co/600x100/png",
     projectLink: "https://example.com/project1"
   },
