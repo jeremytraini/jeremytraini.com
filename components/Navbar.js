39ef930a13a2b07f1d83a5badcaf07959c4b0430
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-11 22:44:03 +1100

    Making navbar separate from NextUI

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,44 +15,14 @@ import clsx from "clsx";
 
 export const Navbar = () => {
 	return (
-		<NextUINavbar
-			maxWidth="xl"
-			position="sticky"
-			className="mx-auto w-fit z-50 radius-full"
+		<div
+			className="flex h-auto items-center justify-center data-[menu-open=true]:border-none sticky top-0 inset-x-0 mx-auto p-5 z-50"
 		>
-			<NavbarContent className="basis-full sm:basis-full radius-full" justify="center">
-				<Tabs radius="full" size="lg">
-					{siteConfig.navItems.map((item, index) => (
-						<Tab key={index} title={item.label} />
-					))}
-				</Tabs>
-				{/* <ul className="hidden lg:flex gap-4 justify-start ml-2">
-					{siteConfig.navItems.map((item, index) => (
-						<NavbarItem
-							key={item.href}
-							className={clsx(
-									"",
-									index === siteConfig.navItems.length - 1 ? "border hover:border-color rounded px-2" : ""
-								)}
-						>
-							<Link
-								className={clsx(
-									linkStyles({ color: "foreground" }),
-									"data-[active=true]:text-primary data-[active=true]:font-medium",
-									index === siteConfig.navItems.length - 1 ? "border hover:border-color rounded px-2" : ""
-								)}
-								color="foreground"
-								href={item.href}
-							>
-								{item.label}
-							</Link>
-						</NavbarItem>
-					))}
-				</ul> */}
-			</NavbarContent>
-			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-									
-			</NavbarContent>
-		</NextUINavbar>
+			<Tabs radius="full" size="lg">
+				{siteConfig.navItems.map((item, index) => (
+					<Tab key={index} title={item.label} />
+				))}
+			</Tabs>
+		</div>
 	);
 };
