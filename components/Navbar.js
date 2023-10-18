debd8c2d0df94f9ec220c172e1bfb6d08cdeb97e
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-18 12:00:00 +1100

    Adding a key prop to the navbar links

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
 		<div className="bg-[#1f1f1f] rounded-full p-1 mx-auto">
 			<ul className="flex justify-center items-center space-x-4">
 				{siteConfig.navItems.map((item, index) => (
-					<a href={item.href}>
+					<a key={index} href={item.href}>
 						<li className="hover:bg-[#292929] rounded-full px-4 py-2 transition text-gray-300">
 							{item.label}
 						</li>
