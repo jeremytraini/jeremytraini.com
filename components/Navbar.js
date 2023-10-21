ecf74c43ba318154d88f00e13666201338422d55
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-21 13:42:25 +1100

    Making front page always span the view height

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,9 +8,15 @@ export const Navbar = () => {
 			<ul className="flex justify-center items-center space-x-4">
 				{siteConfig.navItems.map((item, index) => (
 					<a key={index} href={item.href}>
-						<li className="hover:bg-[#292929] rounded-full px-4 py-2 transition text-gray-300">
-							{item.label}
-						</li>
+						{index === 0 ? (
+							<li className="outline outline-1 outline-[#323232] hover:bg-[#292929] rounded-full px-4 py-2 transition text-gray-300">
+								{item.label}
+							</li>
+						) : (
+							<li className="hover:bg-[#292929] rounded-full px-4 py-2 transition text-gray-300">
+								{item.label}
+							</li>
+						)}
 					</a>
 				))}
 			</ul>
