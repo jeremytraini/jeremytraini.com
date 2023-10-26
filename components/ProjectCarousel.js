bdb634301b10af331bbe00c117207d28e2a9e527
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-26 16:09:23 +1100

    Converting project carousel images to NextJS ones for better optimisation

--- components/ProjectCarousel.js
+++ components/ProjectCarousel.js
@@ -30,6 +30,7 @@ const ProjectCarousel = ({ projects, ...props }) => {
   return (
     <Swiper
       slidesPerView={4}
+      spaceBetween={30}
       centeredSlides={true}
       loop
       breakpoints={{
