1a8c22cb12bca1c4f849ab1a416ae9987b5084bd
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-31 17:02:31 +1100

    Adding proper headline content

--- config/site.js
+++ config/site.js
@@ -1,7 +1,7 @@
 
 export const siteConfig = {
 	name: "Jeremy Traini",
-	description: "My website",
+	description: "Trust me, I’m not a robot… but I speak their language. Pursuing Software Engineering & Commerce at UNSW. Always up for a challenge.",
 	email: "hi@jeremytraini.com",
 	links: {
 		github: "https://github.com/jeremytraini",
