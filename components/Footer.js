4416572da2af01d5fcf7fec68ebff9ae1cd48184
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-10-31 17:07:15 +1100

    Changes to footer content and fixing apostrophes

--- components/Footer.js
+++ components/Footer.js
@@ -8,14 +8,13 @@ export const Footer = () => {
 			<div className="mx-auto max-w-7xl">
 				<div className="flex justify-between items-start py-6">
 					<div>
-						<p className="text-3xl text-white">Feel free to reach out.</p>
-						<p className="text-3xl text-white">I&apos;m open to work.</p>
+						<p className="text-3xl text-white">I&rsquo;m open to work.</p>
+						<p className="text-3xl text-white">Let&rsquo;s have a chat.</p>
 					</div>
 					<div className="flex flex-col gap-2 items-end">
 						<p className="text-xl text-white pb-2">Find me on</p>
 						<Link isExternal href={siteConfig.links.github} showAnchorIcon>GitHub</Link>
 						<Link isExternal href={siteConfig.links.linkedin} showAnchorIcon>LinkedIn</Link>
-						{/* <Link isExternal href={siteConfig.links.dribbble} showAnchorIcon>Dribbble</Link> */}
 					</div>
 				</div>
 				<footer className="w-full flex items-center justify-left py-3">
