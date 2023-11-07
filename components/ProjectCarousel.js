9a589a6d3d89e7de523be59fcc37515ee2e7952c
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-11-07 16:44:26 +1100

    Implementing a function to display most important projects towards centre of carousel

--- components/ProjectCarousel.js
+++ components/ProjectCarousel.js
@@ -25,7 +25,36 @@ const ProjectCarousel = ({ projects, ...props }) => {
     return result;
   }
 
-  const content = wrapAroundList(projects);
+  // Function to make it so that the projects alternate to the left and right of 
+  // the centre one.
+  // e.g. [1, 2, 3, 4, 5] -> [1, 2, 5, 4, 3]
+
+  function alternateList(projects) {
+    let result = [];
+    let addToStart = true;
+
+    for (let i = 0; i < projects.length; i++) {
+      if (addToStart) {
+        result.unshift(projects[i]);
+      } else {
+        result.push(projects[i]);
+      }
+      addToStart = !addToStart;
+    }
+
+    // Rotate the array to the right by half its length
+    // This will put the main project first
+    const halfLength = Math.floor(result.length / 2);
+    for (let i = 0; i <= halfLength; i++) {
+      result.unshift(result.pop());
+    }
+
+    console.log(projects);
+    console.log(result);
+    return result;
+  }
+
+  const content = wrapAroundList(alternateList(projects));
 
   return (
     <Swiper
