5554084892b4602299ac3345e9654d0e5eb2fe68
Jeremy Traini <jeremytraini@gmail.com>
2024-01-12 18:48:10 +1100

    Putting headings sequentially-descending order + name wrapping in timeline

--- components/TimelineItem.js
+++ components/TimelineItem.js
@@ -14,7 +14,7 @@ const icons = {
 
 const TimelineItem = ({ data }) => {
   return (
-    <div className="timeline-item flex space-x-4 my-8" >
+    <div className="timeline-item flex space-x-4 my-8">
       <div className="-ml-2 mt-8 w-4 h-4 bg-default-600 rounded shadow-xl" />
       <div
         className="w-20 h-20 rounded-xl relative shadow-xl flex justify-center items-center"
@@ -24,8 +24,8 @@ const TimelineItem = ({ data }) => {
       </div>
       <div className="flex-1">
         <div>
-          <h3 className="text-xl mb-2">{data.type}</h3>
-          <h3 className="text-xl font-bold mb-2">{data.name}</h3>
+          <h2 className="text-xl mb-2">{data.type}</h2>
+          <h3 className="text-l font-bold mb-2" style={{ whiteSpace: "nowrap" }}>{data.name}</h3>
           <p className="text-gray-500">{data.duration}</p>
           <p className="mt-2 text-gray-700">{data.description}</p>
         </div>
