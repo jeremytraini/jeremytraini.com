82555f945c701df380d2f019be8c3fe4117ad8f0
Jeremy Traini <jeremytraini@gmail.com>
2024-02-02 14:52:33 +1100

    Allowing email me button to work on mobile

--- components/EmailButton.js
+++ components/EmailButton.js
@@ -2,15 +2,17 @@
 
 import React from "react";
 import { Button } from "@nextui-org/button";
+import { Link } from "@nextui-org/link";
 import { siteConfig } from "@/config/site";
 import { MdEmail } from "react-icons/md";
 
 const EmailButton = ({ children, ...props }) => {
   return (
     <Button
+      href={`mailto:${siteConfig.email}?subject=Hello Jeremy!`}
+      as={Link}
       radius="full"
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold"
-      onPress={() => window.open(`mailto:${siteConfig.email}?subject=Hello Jeremy!`)}
       endContent={<MdEmail size={18} />}
       {...props}
     >
