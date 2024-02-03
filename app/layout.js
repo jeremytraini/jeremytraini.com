946228710cea9c24263fd9dd553a1358c81dd1f0
Jeremy Traini <jeremytraini@gmail.com>
2024-02-03 22:42:48 +1100

    Fixing formatting in main layout

--- app/layout.js
+++ app/layout.js
@@ -5,18 +5,18 @@ import { fontSans } from "@/config/fonts";
 import clsx from "clsx";
 
 export const metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
-	],
-	icons: {
-		icon: "/favicon.ico",
-	},
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 
@@ -24,22 +24,23 @@ export default function RootLayout({ children }) {
 
   return (
     <html
-			lang="en"
-			suppressHydrationWarning
-			className="scroll-smooth bg-black"
-		>
-			<head />
-      <body className={clsx(
-					"min-h-screen  antialiased",
-					fontSans.variable
-				)}
-			>
-				<div className="overflow-hidden relative flex flex-col">
-					<main className="w-full pt-10 flex-grow">
-						{children}
-					</main>
-					<Footer />
-				</div>
+      lang="en"
+      suppressHydrationWarning
+      className="scroll-smooth bg-black"
+    >
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen  antialiased",
+          fontSans.variable
+        )}
+      >
+        <div className="overflow-hidden relative flex flex-col">
+          <main className="w-full pt-10 flex-grow">
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   )
