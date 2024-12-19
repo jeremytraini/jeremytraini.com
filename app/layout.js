2654bd31119814a29d31105f2187600ec544be6b
Jeremy Traini <jeremytraini@gmail.com>
2024-12-19 16:20:23 +1100

    Migration to new Vercel speed insights package

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Footer } from "@/components/Footer";
 import { siteConfig } from "@/config/site";
